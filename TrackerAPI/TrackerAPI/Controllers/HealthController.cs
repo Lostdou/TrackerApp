9087001d5e81f7efc1cc8782a8b3da31c1c818a2
Lostdou <facundobott@gmail.com>
2026-01-19 19:27:32 -0300

    Health check añadido en la API

---
+++ TrackerAPI/TrackerAPI/Controllers/HealthController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace TrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HealthController : ControllerBase
+    {
+        private readonly IDbConnection _db;
+
+        public HealthController(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStatus()
+        {
+            var respuesta = new Dictionary<string, object>
+            {
+                { "Estado API", "Conectado" }
+            };
+
+            try
+            {
+                await _db.ExecuteAsync("SELECT 1");
+
+                respuesta.Add("Estado BBDD", "Conectado");
+                respuesta.Add("hora", DateTime.UtcNow);
+
+                return Ok(respuesta);
+            }
+            catch (Exception)
+            {
+                respuesta.Add("Estado BBDD", "No Conectado");
+                respuesta.Add("hora", DateTime.UtcNow);
+
+                return StatusCode(500, respuesta);
+            }
+        }
+    }
+}
